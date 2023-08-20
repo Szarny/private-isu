b85c4f6748c75a981c5893cd13e1d647fec1183e
Tsubasa Umeuchi <tsubasa.umeuchi@gmail.com>
2023-08-20 18:51:46 +0900

    {"pass":true,"score":2181,"success":1903,"fail":0,"messages":[]}

--- benchmarker/run.sh
+++ benchmarker/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-exec "$@"
+docker build -t private-isu-benchmarker .
+docker run --network host -i private-isu-benchmarker /opt/go/bin/benchmarker -t http://host.docker.internal -u /opt/go/userdata
